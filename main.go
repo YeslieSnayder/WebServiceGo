f458c7226f47c9fb57d9fdca88362b6b8e86be57
YeslieSnayder <kuzmickiy2000@mail.ru>
2021-12-24 14:58:33 +0300

    Added configuration by environment variable

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"github.com/YeslieSnayder/WebServiceGo/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
+// How to try it: PORT=8000 go run main.go
 func main() {
 	log.Print("Starting the service...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
 	router := handlers.Router()
 	log.Print("The service is ready to listen and serve")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
