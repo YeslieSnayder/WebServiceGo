829da31f74ffa4263521d434ca7178b875e2e802
YeslieSnayder <kuzmickiy2000@mail.ru>
2021-12-24 14:11:26 +0300

    Added logging

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Print("Starting the service...")
+
 	http.HandleFunc("/home", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "Home handler was awakened.")
 	})
+
+	log.Print("Service is ready to listen and serve")
 	http.ListenAndServe(":8000", nil)
 }
