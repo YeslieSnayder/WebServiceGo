a13811fb2c1be3089fb137913b31b9611e3d08c5
YeslieSnayder <kuzmickiy2000@mail.ru>
2021-12-26 11:22:08 +0300

    Health check

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,10 +2,25 @@ package handlers
 
 import (
 	"github.com/gorilla/mux"
+	"log"
+	"sync/atomic"
+	"time"
 )
 
 func Router(buildTime, commit, release string) *mux.Router {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+
+	go func() {
+		log.Println("Readyz probe is negative by default")
+		time.Sleep(1 * time.Second) // For warming up the cache
+		isReady.Store(true)
+		log.Println("Readyz probe is positive")
+	}()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home(buildTime, commit, release)).Methods("GET")
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
 	return r
 }
